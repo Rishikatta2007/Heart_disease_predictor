869a35dfed3d10bbc12a977b7577e67db557b2c7
Rishikatta2007 <rishikeshkatta2007@gmail.com>
2025-07-17 11:39:30 +0530

    Update streamheart.py

--- streamheart.py
+++ streamheart.py
@@ -1,45 +1,78 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
-import pickle as pk
+import pandas as pd
+import pickle
 
-model=pk.load(open('Heart_disease_model3.pkl','rb'))
+# Load or train mode
+model = pickle.load(open('random_forest_model_1', 'rb'))
 
-data = pd.read_csv('heart_disease.csv')
+# App title
+st.title('Heart Disease Prediction App')
 
-st.header('Heart Disease Predictor')
+# User input section
+st.header('Patient Information')
 
-gender=st.selectbox('Choose Gender',data['Gender'].unique())
-if gender == 'Male':
-    gen = 1
-else:
-    gen = 0
+col1, col2 = st.columns(2)
 
-age = st.number_input("Enter Age")
-currentSmoker = st.number_input("Is patient current Smoker?")
-cigsPerDay = st.number_input("Enter Cigerettes per Day")
-BPMeds = st.number_input("Is Patient on BP Medicines")
-prevalentStroke= st.number_input("Is patient had stroke")
-prevalentHyp = st.number_input("Enter prevalentHyp status")
-diabetes = st.number_input("Enter diabetes status")
-totChol = st.number_input("Enter total cholestrol level")
-sysBP = st.number_input("Enter sysBP")
-diaBP = st.number_input("Enter diaBP")
-BMI = st.number_input("Enter BMI")
-heartRate = st.number_input("Enter heartRate")
-glucose = st.number_input("Enter glucose")
+with col1:
+    age = st.number_input('Age', min_value=1, max_value=120, value=50)
+    sex = st.selectbox('Sex', ['Male', 'Female'])
+    cp = st.selectbox('Chest Pain Type', [
+        'Typical angina', 
+        'Atypical angina', 
+        'Non-anginal pain', 
+        'Asymptomatic'
+    ])
+    trestbps = st.number_input('Resting Blood Pressure (mm Hg)', min_value=80, max_value=200, value=120)
+    chol = st.number_input('Serum Cholesterol (mg/dl)', min_value=100, max_value=600, value=200)
+    fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl', ['No', 'Yes'])
 
+with col2:
+    restecg = st.selectbox('Resting Electrocardiographic Results', [
+        'Normal',
+        'ST-T wave abnormality',
+        'Left ventricular hypertrophy'
+    ])
+    thalach = st.number_input('Maximum Heart Rate Achieved', min_value=60, max_value=220, value=150)
+    exang = st.selectbox('Exercise Induced Angina', ['No', 'Yes'])
+    oldpeak = st.number_input('ST Depression Induced by Exercise', min_value=0.0, max_value=10.0, value=1.0, step=0.1)
+    slope = st.selectbox('Slope of Peak Exercise ST Segment', [
+        'Upsloping',
+        'Flat',
+        'Downsloping'
+    ])
+    ca = st.selectbox('Number of Major Vessels Colored by Fluoroscopy', ['0', '1', '2', '3'])
+    thal = st.selectbox('Thalassemia', [
+        'Normal',
+        'Fixed defect',
+        'Reversible defect'
+    ])
 
-if st.button('Predict'):
-    input = pd.DataFrame([[gen,age,currentSmoker,cigsPerDay,BPMeds,prevalentStroke,prevalentHyp,diabetes,totChol,sysBP,diaBP,BMI,heartRate,glucose]],columns=['Gender', 'age', 'currentSmoker', 'cigsPerDay', 'BPMeds',
-       'prevalentStroke', 'prevalentHyp', 'diabetes', 'totChol', 'sysBP',
-       'diaBP', 'BMI', 'heartRate', 'glucose'])
+# Convert inputs to model format
+sex = 1 if sex == 'Male' else 0
+cp_dict = {'Typical angina': 0, 'Atypical angina': 1, 'Non-anginal pain': 2, 'Asymptomatic': 3}
+cp = cp_dict[cp]
+fbs = 1 if fbs == 'Yes' else 0
+restecg_dict = {'Normal': 0, 'ST-T wave abnormality': 1, 'Left ventricular hypertrophy': 2}
+restecg = restecg_dict[restecg]
+exang = 1 if exang == 'Yes' else 0
+slope_dict = {'Upsloping': 0, 'Flat': 1, 'Downsloping': 2}
+slope = slope_dict[slope]
+ca = int(ca)
+thal_dict = {'Normal': 1, 'Fixed defect': 2, 'Reversible defect': 3}
+thal = thal_dict[thal]
 
-    output = model.predict(input)
+# Create feature array
+features = [[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]]
 
-    if output[0] == 0:
-        stn = 'Patient is Healthy, No need to worry'
+# Make prediction
+if st.button('Predict Heart Disease'):
+    prediction = model.predict(features)
+    probability = model.predict_proba(features)
+    
+    st.subheader('Prediction Result')
+    if prediction[0] == 1:
+        st.error(f'High risk of heart disease ({probability[0][1]*100:.2f}% probability)')
     else:
-        stn = 'Patient seems to have heart disease, please consult to the doctor!'
-
-    st.write(stn)
+        st.success(f'Low risk of heart disease ({probability[0][0]*100:.2f}% probability)')
+    
+    
